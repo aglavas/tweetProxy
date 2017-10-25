069f7ba1209c8fe0e31ed6cd72861ae4037c0117
Andrija Glavaš <aglavas11@gmail.com>
2017-10-25 15:47:57 +0200

    Tests fix format

--- tests/AppBundle/Controller/ControllersDependencyTest.php
+++ tests/AppBundle/Controller/ControllersDependencyTest.php
@@ -6,73 +6,71 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ControllersDependencyTest extends WebTestCase
 {
-	public function testIndex()
+    public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertGreaterThan(0, $crawler->filter('a')->count() );
+        $this->assertGreaterThan(0, $crawler->filter('a')->count());
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $username_uri = $crawler->filter('a')->attr('href');
 
-		return $username_uri;
+        return $username_uri;
     }
 
 
-	/**
-	 * @depends testIndex
-	 */
+    /**
+     * @depends testIndex
+     */
     public function testUsername($username_uri)
-	{
-		$client = static::createClient();
+    {
+        $client = static::createClient();
 
-		$crawler = $client->request('GET', $username_uri );
+        $crawler = $client->request('GET', $username_uri);
 
-		$this->assertGreaterThan(0, $crawler->filter('li')->count() );
+        $this->assertGreaterThan(0, $crawler->filter('li')->count());
 
-		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-		$text = $crawler->filter('li')->text();
+        $text = $crawler->filter('li')->text();
 
-		$words = str_word_count($text, 1);
+        $words = str_word_count($text, 1);
 
-		foreach ($words as $word)
-		{
-			//Full text search supports words longer than 3 characters, possible to override in php.ini
-			//https://stackoverflow.com/questions/1585611/mysql-full-text-search-for-words-with-three-or-less-letters
-			if( strlen($word) > 3)
-			{
-				return $word;
-			}
-		}
+        foreach ($words as $word) {
+            //Full text search supports words longer than 3 characters, possible to override in php.ini
+            //https://stackoverflow.com/questions/1585611/mysql-full-text-search-for-words-with-three-or-less-letters
+            if (strlen($word) > 3) {
+                return $word;
+            }
+        }
 
-		return $words[0];
-	}
+        return $words[0];
+    }
 
 
-	/**
-	 * @depends testUsername
-	 */
-	public function testSearch($tweet_text)
-	{
-		$client = static::createClient();
+    /**
+     * @depends testUsername
+     */
+    public function testSearch($tweet_text)
+    {
+        $client = static::createClient();
 
-		$crawler = $client->request('GET', 'search');
+        $crawler = $client->request('GET', 'search');
 
-		$form = $crawler->filter('form')->form();
+        $form = $crawler->filter('form')->form();
 
-		$form->setValues(array(
-			"tweet[text]" => $tweet_text,
-			"tweet[userId]"  => ""
-		));
+        $form->setValues(array(
+            "tweet[text]" => $tweet_text,
+            "tweet[userId]"  => ""
+        ));
 
-		$crawler = $client->submit($form);
+        $crawler = $client->submit($form);
 
-		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-		$this->assertEquals(1, $crawler->filter('div#results')->count());
-	}
+        $this->assertEquals(1, $crawler->filter('div#results')->count());
+    }
 }
--- tests/AppBundle/Repository/TweetRepositoryTest.php
+++ tests/AppBundle/Repository/TweetRepositoryTest.php
@@ -8,56 +8,58 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TweetRepositoryTest extends KernelTestCase
 {
-	/**
-	 * @var TweetRepository
-	 */
-	private $tweetRepository;
+    /**
+     * @var TweetRepository
+     */
+    private $tweetRepository;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected function setUp()
-	{
-		self::bootKernel();
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
 
-		$this->tweetRepository = static::$kernel->getContainer()
-			->get('doctrine')
-			->getManager()
-			->getRepository(Tweet::class);
-	}
+        $this->tweetRepository = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager()
+            ->getRepository(Tweet::class);
+    }
 
-	public function testFullTextSearchQueryBuilderWithBothParameters()
-	{
-		$result = $this->tweetRepository->fullTextSearch(
-			[
-				'tweetText' => 'aaa',
-				'userId'    => 2,
-			]);
+    public function testFullTextSearchQueryBuilderWithBothParameters()
+    {
+        $result = $this->tweetRepository->fullTextSearch(
+            [
+                'tweetText' => 'aaa',
+                'userId'    => 2,
+            ]
+        );
 
-		$this->assertEquals('SELECT t, t.text, u.name, u.screenName, MATCH_AGAINST (t.text, :tweet) as score FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE MATCH_AGAINST(t.text, :tweet) > 0 AND t.user = :user_id', $result->getDQL());
-	}
+        $this->assertEquals('SELECT t, t.text, u.name, u.screenName, MATCH_AGAINST (t.text, :tweet) as score FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE MATCH_AGAINST(t.text, :tweet) > 0 AND t.user = :user_id', $result->getDQL());
+    }
 
-	public function testFullTextSearchQueryBuilderWithTweetTextParameter()
-	{
-		$result = $this->tweetRepository->fullTextSearch(
-			[
-				'tweetText' => 'aaa',
-				'userId'    => null,
-			]);
+    public function testFullTextSearchQueryBuilderWithTweetTextParameter()
+    {
+        $result = $this->tweetRepository->fullTextSearch(
+            [
+                'tweetText' => 'aaa',
+                'userId'    => null,
+            ]
+        );
 
-		$this->assertEquals('SELECT t, t.text, u.name, u.screenName, MATCH_AGAINST (t.text, :tweet) as score FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE MATCH_AGAINST(t.text, :tweet) > 0', $result->getDQL());
-	}
+        $this->assertEquals('SELECT t, t.text, u.name, u.screenName, MATCH_AGAINST (t.text, :tweet) as score FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE MATCH_AGAINST(t.text, :tweet) > 0', $result->getDQL());
+    }
 
 
-	public function testFullTextSearchQueryBuilderWithUserIdParameter()
-	{
-		$result = $this->tweetRepository->fullTextSearch(
-			[
-				'tweetText' => null,
-				'userId'    => 2,
-			]);
+    public function testFullTextSearchQueryBuilderWithUserIdParameter()
+    {
+        $result = $this->tweetRepository->fullTextSearch(
+            [
+                'tweetText' => null,
+                'userId'    => 2,
+            ]
+        );
 
-		$this->assertEquals('SELECT t, t.text, u.name, u.screenName FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE t.user = :user_id', $result->getDQL());
-	}
-
+        $this->assertEquals('SELECT t, t.text, u.name, u.screenName FROM AppBundle\Entity\Tweet t INNER JOIN AppBundle:User u WITH t.user = u.id WHERE t.user = :user_id', $result->getDQL());
+    }
 }
